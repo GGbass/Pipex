500a479d50c8b3e0f5435de49c86b656583cb8cd
GGbass <102612939+GGbass@users.noreply.github.com>
2024-06-11 23:14:37 +0200

    Add files via upload

---
+++ pipex/pipex/get_path.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_path.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/05 21:14:22 by marvin            #+#    #+#             */
+/*   Updated: 2024/06/05 21:14:22 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex_bonus.h"
+
+char	*find_path(char *cmd, char **envp)
+{
+	char	**paths;
+	char	*goodpath;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (cmd[0] == '/' && access(cmd, 0) == 0)
+		return (cmd);
+	while (envp[i] && ft_strncmp(envp[i], "PATH=", 5))
+		i++;
+	paths = ft_split(envp[i] + 5, ':');
+	cmd = ft_strjoin("/", cmd);
+	while (paths[j])
+	{
+		goodpath = ft_strjoin(paths[j], cmd);
+		if (access(goodpath, 0) == 0)
+			return (goodpath);
+		j++;
+	}
+	ft_error("Error command not found\n", 127, NULL);
+	return (NULL);
+}
---
+++ pipex/pipex/pipex_bonus.h
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gongarci <gongarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/13 18:39:21 by gongarci          #+#    #+#             */
+/*   Updated: 2024/06/11 21:18:51 by gongarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
+# include "./libft/libft.h"
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+
+# define READ 0
+# define WRITE 1
+
+typedef struct s_values
+{
+	char	**env;
+	char	**cmd;
+	char	**doc;
+	int		check;
+}	t_values;
+
+typedef struct s_pipex
+{
+	pid_t	pid;
+	int		*pipe_fd;
+	int		*pre_pipe;
+	int		i;
+	int		status;
+}	t_pipex;
+
+/* Creates a set of fds and forks to pass the output to a new command */
+/* int		pipex(int *fd, char **env, char **cmd, int cmd_len); */
+int		pipex(int *fd, t_values *vals, int cmd_len);
+/*  Find the path of a command in the environment*/
+char	*find_path(char *cmd, char **env);
+
+void	ft_error(char *message, int status, t_pipex *pipex);
+
+void	ft_error2(char *message, int status);
+
+char	**get_here_doc(char *limiter);
+
+//char **get_doc_array(char *limiter, char *doc);
+
+int		checker(char *argv);
+
+void	ft_cleanup(t_pipex *pipex);
+
+void ft_fd(t_pipex *pipex);
+
+
+#endif
